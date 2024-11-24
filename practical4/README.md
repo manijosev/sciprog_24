0805527ebfd75d6eed24e011ae66d16688240d78
mani jose <manijosev@live.com>
2024-11-24 12:38:24 +0000

    practical 4 comibed code added

--- practical4/README.md
+++ practical4/README.md
@@ -5,11 +5,13 @@ This subdirectory contains the code in C for practical 4.
 ## Files Included
 1. TanArray.c  - Creates an array of tan values of [0,60] degrees inclusive of both
 2. TrapFunction.c -  The trapezoidal rule, written as functions
+3. main.c - The program combining functionalities of both.
 
 ## Compile Commands
 
 * gcc -o TanArray TanArray.c -lm
 * gcc -o TrapFunction TrapFunction.c -lm
+* gcc -o main main.c -lm
 
 ## Execution commands and Outputs
 
@@ -37,5 +39,25 @@ Elements of the array are as follows :
 1.000000
 1.191754
 1.428148
+```
+* ./main
+
+```
+Tan[0]=0.000000
+Tan[1]=0.087489
+Tan[2]=0.176327
+Tan[3]=0.267949
+Tan[4]=0.363970
+Tan[5]=0.466308
+Tan[6]=0.577350
+Tan[7]=0.700208
+Tan[8]=0.839100
+Tan[9]=1.000000
+Tan[10]=1.191754
+Tan[11]=1.428148
+
+Computed Integral using trapezoidal rule :0.619470
+Actual value is:0.693147
+
 ```
 
---
+++ practical4/main.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<math.h>
+#define N 12
+
+float TanArr[N+1];
+float degToRad(float degarg);
+float trapRule();
+int main(){
+        int i;
+        float a=0.0,b_deg=60.0;
+        float deg;
+
+        for(i=0;i<N;i++){
+                deg=i*5.0;
+                TanArr[i]=tan(degToRad(deg));
+                printf("Tan[%d]=%f\n",i,TanArr[i]);
+        }
+
+        float area=trapRule();
+        printf("\nComputed Integral using trapezoidal rule :%f\n",area);
+        printf("Actual value is:%f\n",log(2.0));
+        return 0;
+}
+float degToRad(float degarg){
+        float pi=3.1415927;
+        return((pi*degarg)/180.0);
+}
+float trapRule(){
+        float area;
+        int i;
+        area=TanArr[0]+TanArr[N];
+        for(i=i;i<N;i++){
+                area=area+2*TanArr[i];
+        }
+        float mult_rad=degToRad((60.0-0.0)/(2*N));
+        area=mult_rad*area;
+        return area;
+}
